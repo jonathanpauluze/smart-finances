eace0481ed1ab8253c46a0508ac147c98333438c
Jonathan Pauluze <jonathanpauluze@gmail.com>
2021-03-22 23:02:48 -0300

    feat: create Dashboard component

---
+++ src/components/Dashboard/index.tsx
@@ -0,0 +1,11 @@
+import { Summary } from '../Summary';
+import { DashboardContainer } from './styles';
+
+export function Dashboard() {
+  return (
+    <DashboardContainer>
+      <Summary />
+    </DashboardContainer>
+  );
+  
+}
