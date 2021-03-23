e49ff55e1f3049464afbab8126177191ca3ae469
Jonathan Pauluze <jonathanpauluze@gmail.com>
2021-03-22 23:02:35 -0300

    feat: create Summary component

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,38 @@
+import { SummaryContainer } from './styles';
+
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
+import totalImg from '../../assets/total.svg';
+
+export function Summary() {
+  return (
+    <SummaryContainer>
+      <div>
+        <header>
+          <p>Entradas</p>
+          <img src={incomeImg} alt="Entradas" />
+        </header>
+
+        <strong>R$ 1.000,00</strong>
+      </div>
+
+      <div>
+        <header>
+          <p>Saídas</p>
+          <img src={outcomeImg} alt="Saídas" />
+        </header>
+
+        <strong>- R$ 200,00</strong>
+      </div>
+
+      <div className="hightlight-background">
+        <header>
+          <p>Total</p>
+          <img src={totalImg} alt="Total" />
+        </header>
+
+        <strong>R$ 800,00</strong>
+      </div>
+    </SummaryContainer>
+  );
+}
