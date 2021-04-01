cad1df951b165efedb8e1cd76a65f6d68d026bb3
Jonathan Pauluze <jonathanpauluze@gmail.com>
2021-03-31 22:30:26 -0300

    add onClick event to open transaction modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,13 +2,17 @@ import { HeaderContainer, HeaderContent } from './styles';
 
 import logo from '../../assets/logo.svg';
 
-export function Header() {
+interface HeaderProps {
+  onOpenNewTransactionModal: () => void;
+}
+
+export function Header({ onOpenNewTransactionModal }: HeaderProps) {
   return (
     <HeaderContainer>
       <HeaderContent>
         <img src={logo} alt="Smart Finances" />
 
-        <button type="button">
+        <button type="button" onClick={onOpenNewTransactionModal}>
           Nova transação
         </button>
       </HeaderContent>
