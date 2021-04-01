0932aa679dab19a7431047e189ddfe9541880a90
Jonathan Pauluze <jonathanpauluze@gmail.com>
2021-03-31 22:30:06 -0300

    fix: TransactionTable using dynamic data

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,6 +1,12 @@
+import { useContext } from 'react';
+
+import { TransactionsContext } from '../../TransactionsContext';
+
 import { TransactionsTableContainer } from './styles';
 
 export function TransactionsTable() {
+  const { transactions } = useContext(TransactionsContext);
+  
   return (
     <TransactionsTableContainer>
       <table>
@@ -14,26 +20,23 @@ export function TransactionsTable() {
         </thead>
 
         <tbody>
-          <tr>
-            <td>Desenvolvimento de Website</td>
-            <td className="deposit">+ R$ 12.000,00</td>
-            <td>Desenvolvimento</td>
-            <td>20/01/2021</td>
-          </tr>
-
-          <tr>
-            <td>Sofá 2 lugares</td>
-            <td className="withdraw">- R$ 750,00</td>
-            <td>Casa</td>
-            <td>19/01/2021</td>
-          </tr>
-
-          <tr>
-            <td>Bonificação do trabalho</td>
-            <td className="deposit">+ R$ 1.500,00</td>
-            <td>Trabalho</td>
-            <td>18/01/2021</td>
-          </tr>
+          {transactions.map((transaction) => (
+            <tr key={transaction.id}>
+              <td>{transaction.title}</td>
+              <td className={transaction.type}>
+                {new Intl.NumberFormat('pt-BR', {
+                  style: 'currency',
+                  currency: 'BRL',
+                }).format(transaction.amount)}
+              </td>
+              <td>{transaction.category}</td>
+              <td>
+                {new Intl.DateTimeFormat('pt-BR').format(
+                  new Date(transaction.createdAt)
+                )}
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </TransactionsTableContainer>
