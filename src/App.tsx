a3a1424ce3bfbd26db48c69bcaf33285c1431864
Jonathan Pauluze <jonathanpauluze@gmail.com>
2021-03-31 22:30:57 -0300

    use transaction modal in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,36 @@
+import { useState } from 'react';
+
 import { Header } from './components/Header';
 import { Dashboard } from './components/Dashboard';
+import { NewTransactionModal } from './components/NewTransactionModal';
+
+import { TransactionsProvider } from './TransactionsContext';
+
 import { GlobalStyle } from './styles/global';
 
+
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
+
   return (
-    <>
-      <Header />
+    <TransactionsProvider>
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
+
       <GlobalStyle />
-    </>
+    </TransactionsProvider>
   );
 }
 
